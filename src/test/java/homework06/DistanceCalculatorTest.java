4c63eedcfe892785d9a95720e89d3a75b27c337b
AntonioCucu <cu.antonio53@gmail.com>
2022-07-24 10:27:27 +0300

    Sixth homework. Added random numbers unit test.

--- src/test/java/homework06/DistanceCalculatorTest.java
+++ src/test/java/homework06/DistanceCalculatorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import siit.homework06.DistanceCalculator;
 
+import java.util.Random;
+
 public class DistanceCalculatorTest {
 
     @Test
@@ -32,5 +34,21 @@ public class DistanceCalculatorTest {
         DistanceCalculator calculator = new DistanceCalculator();
 
         Assertions.assertEquals(115, calculator.inputCalculator("250 mm - 10 cm + 1 m", "cm"));
+    }
+
+    @Test
+    public void calculator_test_random() {
+
+        Random r = new Random();
+        int rand1 = r.nextInt(100) + 1;
+        int rand2 = r.nextInt(100) + 1;
+        int rand3 = r.nextInt(100) + 1;
+
+        String equation = rand1 + " cm + " + rand2 + " m + " + rand3 + " mm";
+
+        DistanceCalculator calculator = new DistanceCalculator();
+
+        Assertions.assertEquals(rand1 + rand2 * 100 + rand3 * 0.1, calculator.inputCalculator(equation, "cm"));
+
     }
 }
